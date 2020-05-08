a06802066228d70db4eb4f089bed888aa7608db2
Liu-Taiting <liutaiting@qq.com>
2020-05-08 12:55:34 +0800

    dataset, config

--- core/config.py
+++ core/config.py
@@ -3,7 +3,7 @@
 @author: liutaiting
 @lastEditors: liutaiting
 @Date: 2020-05-05 10:55:07
-@LastEditTime: 2020-05-05 11:09:00
+@LastEditTime: 2020-05-07 00:00:15
 '''
 
 from easydict import EasyDict as edict
@@ -20,3 +20,28 @@ __C.YOLO.ANCHORS              = "./data/anchors/basline_anchors.txt"
 __C.YOLO.STRIDES              = [8, 16, 32]
 __C.YOLO.ANCHOR_PER_SCALE     = 3
 __C.YOLO.IOU_LOSS_THRESH      = 0.5
+
+# Train options
+__C.TRAIN                     = edict()
+
+__C.TRAIN.ANNOT_PATH          = "./data/dataset/yymnist_train.txt"
+__C.TRAIN.BATCH_SIZE          = 5
+# __C.TRAIN.INPUT_SIZE            = [320, 352, 384, 416, 448, 480, 512, 544, 576, 608]
+__C.TRAIN.INPUT_SIZE          = [416]
+__C.TRAIN.DATA_AUG            = True
+__C.TRAIN.LR_INIT             = 1e-3
+__C.TRAIN.LR_END              = 1e-6
+__C.TRAIN.WARMUP_EPOCHS       = 2
+__C.TRAIN.EPOCHS              = 30
+
+
+# TEST options
+__C.TEST                      = edict()
+
+__C.TEST.ANNOT_PATH           = "./data/dataset/yymnist_test.txt"
+__C.TEST.BATCH_SIZE           = 2
+__C.TEST.INPUT_SIZE           = 544
+__C.TEST.DATA_AUG             = False
+__C.TEST.DECTECTED_IMAGE_PATH = "./data/detection/"
+__C.TEST.SCORE_THRESHOLD      = 0.3
+__C.TEST.IOU_THRESHOLD        = 0.45
